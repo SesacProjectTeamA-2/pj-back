78aa6752e947691662f15fc1af8cdcb9d7aa0790
eoeung <eoeung113@gmail.com>
2023-10-26 18:01:35 +0900

    feat ::: 구글 로그인 완성

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const controller = require('../controller/Cuser');
 router.get('/users', controller.getUsers); // 모든 유저 조회
 router.get('/login/google', controller.getLoginGoogle); // 구글 로그인
 router.get('/login/google/redirect', controller.getLoginGoogleRedirect); // 구글 로그인 처리
+router.post('/signup', controller.postSignUp); // 회원가입
 
 module.exports = router;
