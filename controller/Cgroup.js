7496419b1c140c6410df85ec8c3f222ee9e956df
chitty12 <cty12angel@naver.com>
2023-10-30 18:40:36 +0900

    feat/groupMain : 회원/비회원/ 모임가입여부/ 모임장 여부

--- controller/Cgroup.js
+++ controller/Cgroup.js
@@ -1,39 +1,32 @@
 const dotenv = require('dotenv');
-const axios = require('axios');
 dotenv.config({ path: __dirname + '/../config/.env' });
 const { User, Group, GroupUser, Mission } = require('../models');
 const { Op } = require('sequelize');
 
 // 로그인 된 사용자인지 아닌지 판별하려면 불러와야함
 const jwt = require('../modules/jwt');
-const authUtil = require('../middlewares/auth');
+const secretKey = require('../config/secretkey');
 
 // 모임 페이지 load
 exports.getGroup = async (req, res) => {
-  authUtil.checkToken(req, tokenRes);
-  console.log(tokenRes.user);
-
   const groupSeq = req.params.gSeq;
-
-  // 모임장여부 : true/false
-  const groupUser = await GroupUser.findOne({
-    attributes: ['guIsLeader'],
-    where: { gSeq: groupSeq, uSeq: tokenRes.user.uSeq },
-  });
-  const isLeader = groupUser && groupUser.guIsLeader === 'y';
 
   // 모임 정보
   const groupInfo = await Group.findOne({ where: { gSeq: groupSeq } });
   const groupMission = await Mission.findAll({
     where: { gSeq: groupSeq, guisBlackUser: { [Op.ne]: 'y' } },
   });
-  const members = await GroupUser.findAll({ where: { gSeq: groupSeq } });
-  const uSeqArray = members.map((groupUser) => {
-    groupUser.uSeq;
-  });
   const groupMem = await User.findAll({
-    where: { uSeq: uSeqArray },
+    include: [
+      {
+        model: GroupUser,
+        where: { gSeq: groupSeq },
+      },
+    ],
+    attributes: ['uName'],
   });
+
+  console.log(groupMem);
 
   const { gName, gDesc, gDday, gCategory, gCoverImg } = groupInfo;
   const missionTitle = groupMission.map((mission) => {
@@ -43,17 +36,57 @@ exports.getGroup = async (req, res) => {
     gr.mContent;
   });
 
-  res.send({
-    isLeader,
-    missionTitle,
-    missionContent,
-    groupMem,
-    groupName: gName,
-    groupInfo: gDesc,
-    groupDday: gDday,
-    groupCategory: gCategory,
-    groupCoverImg: gCoverImg,
-  });
+  // 회원인경우
+  if (req.headers) {
+    let token = req.headers.authorization.split('')[1];
+    const userInfo = await jwt.verify(token);
+    console.log('토큰 디코딩>>>>>', userInfo);
+
+    // 모임가입여부
+    let isMember = await GroupUser.findOne({
+      attributes: ['guIsLeader'],
+      where: { gSeq: groupSeq, uSeq: userInfo.uSeq },
+    });
+
+    if (isMember) {
+      let isLeader = isMember && isMember.guIsLeader === 'y';
+
+      return { isLeader, isMember: true };
+      // 모임장여부 : true/false
+    } else {
+      isMember = false;
+    }
+    console.log('리더여부', isLeader);
+    console.log('멤버여부', isMember);
+
+    res.send({
+      isLogin: true,
+      isMember,
+      isLeader,
+      missionTitle,
+      missionContent,
+      groupMem,
+      groupName: gName,
+      groupInfo: gDesc,
+      groupDday: gDday,
+      groupCategory: gCategory,
+      groupCoverImg: gCoverImg,
+    });
+  }
+  // 비회원인경우
+  else {
+    res.send({
+      isLogin: false,
+      missionTitle,
+      missionContent,
+      groupMem,
+      groupName: gName,
+      groupInfo: gDesc,
+      groupDday: gDday,
+      groupCategory: gCategory,
+      groupCoverImg: gCoverImg,
+    });
+  }
 };
 
 exports.joinGroup = async (req, res) => {
@@ -78,13 +111,14 @@ exports.joinGroup = async (req, res) => {
 
 exports.rankSystem = async (req, res) => {};
 
-// 랭킹 : 점수 = 완료개수 * 순서
-// gSeq, uSeq로 특정, mSeq 에 대한 점수는
-// 1. 모임의 미션 중 완료여부 y 인 것 추출 => updatedAt 으로 오름차순(과거부터 현재순)
-// 2. 점수 부여 : 모임 참여 유저의 gSeq 수가 해당 미션의 만점 : 이후 user는 for 문 -1 로 값 정의
-// 3. mSeq 반복
-// 4. 해당 uSeq 가진 값들의 총합 = 해당 유저의 총 점수
-// 5. 점수 순으로 내림차순 = 랭킹
+// 미션 점수(tb_group - mLevel) : 상=>5 /중=>3 /하=>1
 
-// 만점 = gSeq의 mSeq의 개수 * 모임참여 유저의 gSeq 수
-// 퍼센트 = uSeq의 총 점수/ 만점 * 100
+// 회원별 현재 점수(tb_groupUser : guNowScore)
+// => 미션완료시의 점수(tb_groupBoard: gSeq, mSeq, uSeq, gbIsDone(y) Join tb_mission(mSeq) = mLevel 총 합) + 회원의 현재점수(tb_groupUser : guNowScore)
+
+// 회원별 누적 점수(tb_groupUser : guTotalScore)
+// => Dday 종료시 guNowScore + guTotalScore
+// [guNowScore, gTotalScore 모두 초기화/ 모임 미션 만료]
+
+// 모임 미션 총 점수(tb_group : gTotalScore) : 모든 미션의 점수 총 합
+//  => gSeq의 mLevel 추출하여 모두 합한 값
