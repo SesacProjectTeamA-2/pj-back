d007677e7e8497fdd1acd0a9a9d22a1a17f066dd
eoeung <eoeung113@gmail.com>
2023-10-31 15:26:40 +0900

    feat ::: 모임 수정 완료 / 모임 삭제 진행중 -> 모든 모임 컨텐츠가 완료되어야 가능할 듯

--- routes/group.js
+++ routes/group.js
@@ -12,22 +12,73 @@ const authUtil = require('../middlewares/auth').checkToken;
  *       description: 모임 생성
  *       tags: [Group]
  *       requestBody:
- *         description: 모임을 생성하기 위한 정보
+ *         description: 모임을 생성하기 위해 필요한 정보
+ *         required: true
  *         content:
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/postGroup'
- *         required: true
  *       responses:
  *         "200":
- *           description: 생성 완료된 모임 정보
+ *           description: 모임 생성에 대한 성공 여부/메시지
+ *           required: true
  *           content:
  *             application/json:
  *               schema:
- *                 $ref: '#/components/schemas/postGroupResult'
+ *                 $ref: '#/components/schemas/groupApiResult'
  */
 router.post('/', authUtil, controller.postGroup); // 모임 생성
 
+/**
+ * @swagger
+ * paths:
+ *   /api/group/:
+ *     patch:
+ *       summary: 모임 수정
+ *       description: 모임 수정
+ *       tags: [Group]
+ *       requestBody:
+ *         description: 모임을 수정하기 위해 필요한 정보
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/patchGroup'
+ *       responses:
+ *         "200":
+ *           description: 모임 수정에 대한 성공 여부/메시지
+ *           required: true
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/groupApiResult'
+ */
+router.patch('/', authUtil, controller.patchGroup); // 모임 수정
 
+/**
+ * @swagger
+ * paths:
+ *   /api/group/:
+ *     delete:
+ *       summary: 모임 삭제
+ *       description: 모임 삭제
+ *       tags: [Group]
+ *       requestBody:
+ *         description: 모임을 삭제하기 위해 필요한 정보
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/deleteGroup'
+ *       responses:
+ *         "200":
+ *           description: 모임 삭제에 대한 성공 여부/메시지
+ *           required: true
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/groupApiResult'
+ */
+router.delete('/', authUtil, controller.deleteGroup); // 모임 삭제
 
 module.exports = router;
