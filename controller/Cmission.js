0144e5707daf1a0c6ec239a59abd23568c4f0410
chitty12 <cty12angel@naver.com>
2023-11-01 18:02:35 +0900

    feat/missionUpdate

--- controller/Cmission.js
+++ controller/Cmission.js
@@ -8,22 +8,30 @@ const { Op } = require('sequelize');
 
 // 로그인 된 사용자인지 아닌지 판별하려면 불러와야함
 const jwt = require('../modules/jwt');
-const authUtil = require('../middlewares/auth');    
+const authUtil = require('../middlewares/auth');
 
 cron.schedule('01 00 * * *', async () => {
-  await Group.update({
-    gDday: Sequelize.literal('gDday-1'),
-    where: { gDday: { [Op.gte]: 0 } },
+  await Group.update(
+    {
+      gDday: Sequelize.literal('gDday-1'),
+    },
+    { where: { gDday: { [Op.gte]: 0 } } }
+  );
+
+  const exGroups = await Group.findAll({
+    where: { gDday: 0 },
+    include: [{ model: 'Mission', where: { isExpired: { [Op.not]: 'y' } } }],
+    attributes: ['uSeq'],
   });
 
-const exGroups = await Group.findAll({where: {gDday: 0},
-    include:[
-        {model: 'Mission',
-    where: {isExpired: {[Op.not]: 'y'}}
-}]
-})
+  console.log(exGroups.gSeq);
 
-const groupMission = await exGroups.map((gr) => {gr.isExpired})
-
-  if()
+  await Mission.update(
+    {
+      isExpired: 'y',
+    },
+    {
+      where: { gSeq: exGroups.gSeq },
+    }
+  );
 });
