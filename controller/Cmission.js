b71a385a71f99c4d43e80bee09248a8186d1d03f
chitty12 <cty12angel@naver.com>
2023-11-04 22:52:59 +0900

    fix/user/mission : swagger 수정 및 user, mission 에러 수정

--- controller/Cmission.js
+++ controller/Cmission.js
@@ -5,6 +5,7 @@ dotenv.config({ path: __dirname + '/../config/.env' });
 const { User, Group, Mission, GroupBoard, GroupUser } = require('../models');
 const sequelize = require('sequelize');
 const { Op } = require('sequelize');
+const score = require('../modules/rankSystem');
 
 // 로그인 된 사용자인지 아닌지 판별하려면 불러와야함
 const jwt = require('../modules/jwt');
@@ -28,11 +29,11 @@ cron.schedule('1 0 * * *', async () => {
           [Op.lte]: currentDate.toISOString().slice(0, 10), // 현재 날짜 이전인 경우
         },
       },
-      include: [{ model: 'Mission', where: { isExpired: { [Op.ne]: 'y' } } }],
+      include: [{ model: Mission, where: { isExpired: { [Op.is]: null } } }],
       attributes: ['gSeq'],
     });
 
-    if (Groups) {
+    if (Groups.length > 0) {
       for (const group of Groups) {
         // 미션 만료
         await Mission.update(
@@ -42,9 +43,12 @@ cron.schedule('1 0 * * *', async () => {
         // 누적 점수 업데이트, 현재 점수 초기화
         await GroupUser.update(
           {
-            guNowScore: 0,
             guTotalScore: sequelize.literal('guTotalScore+guNowScore'),
           },
+          { where: { gSeq: group.gSeq } }
+        );
+        await GroupUser.update(
+          { guNowScore: 0 },
           { where: { gSeq: group.gSeq } }
         );
         await Group.update({ gTotalScore: 0 }, { where: { gSeq: group.gSeq } });
@@ -69,25 +73,31 @@ exports.getMission = async (req, res) => {
   if (req.headers.authorization) {
     const token = req.headers.authorization.split(' ')[1];
     const user = await jwt.verify(token);
-
+    const uSeq = user.uSeq;
     // 2. 유저 닉네임/캐릭터
     const userInfo = await User.findOne({
-      where: { uSeq: user.uSeq },
+      where: { uSeq },
     });
     const { uName, uCharImg } = userInfo;
 
     // 3. 그룹별 미션 load(), group [디데이, 모임명 - join], mission [미션 제목, 미션만료x(null)], group board[미션완료여부(y) mission join]
     const groupInfo = await GroupUser.findAll({
-      where: { uSeq: user.uSeq },
+      where: { uSeq },
       attributes: ['gSeq'],
       include: [{ model: Group, attributes: ['gName', 'gDday'] }],
     });
 
     const gSeqArray = groupInfo.map((group) => group.gSeq);
 
+    // 그룹별 달성률
+    const groupDoneRates = [];
+    for (const groupSeq of gSeqArray) {
+      score.doneRate(groupSeq, uSeq);
+    }
+
     const missionArray = await Mission.findAll({
       attributes: ['mSeq', 'gSeq', 'mTitle'],
-      where: { gSeq: { [Op.in]: gSeqArray }, isExpired: { [Op.ne]: 'y' } },
+      where: { gSeq: { [Op.in]: gSeqArray }, isExpired: { [Op.is]: null } },
     });
 
     const doneArray = await GroupBoard.findAll({
@@ -105,6 +115,7 @@ exports.getMission = async (req, res) => {
       groupInfo,
       isDone: isDoneArray,
       missionArray,
+      groupDoneRates,
     });
   } else {
     res.json({ result: false, message: '로그인 해주세요!' });
--- modules/rankSystem.js
+++ modules/rankSystem.js
@@ -12,48 +12,66 @@ const {
 } = require('../models');
 const Op = require('sequelize').Op;
 const sequelize = require('sequelize');
-const jwt = require('../modules/jwt');
 
 module.exports = {
   currentScore: async (guSeq, mSeq) => {
-    // 게시글 작성시 => 현재 점수에 미션 난이도에 따른 점수 추가
-    const score = await Mission.findOne({
-      where: { mSeq: mSeq, isExpired: { [Op.ne]: 'y' } },
-      attributes: ['mLevel'],
-    });
+    try {
+      // 게시글 작성시 => 현재 점수에 미션 난이도에 따른 점수 추가
+      const score = await Mission.findOne({
+        where: { mSeq: mSeq, isExpired: { [Op.is]: null } },
+        attributes: ['mLevel'],
+      });
 
-    await GroupUser.update(
-      {
-        guNowScore: sequelize.literal(`guNowScore + ${score}`),
-      },
-      { where: { guSeq: guSeq } }
-    );
+      await GroupUser.update(
+        {
+          guNowScore: sequelize.literal(`guNowScore + ${score.mLevel}`),
+        },
+        { where: { guSeq } }
+      );
+    } catch (error) {
+      if (err.message === 'jwt expired') {
+        console.log('expired token');
+        return TOKEN_EXPIRED;
+      }
+    }
   },
 
   doneRate: async (gSeq, uSeqArray) => {
-    // 달성률
-    // 현재 점수 % 그룹 미션 총 점수 *100
-    // 그룹 미션 총 점수
-    const missionTotal = await Group.findOne({
-      where: { gSeq: gSeq },
-      attributes: ['gTotalScore'],
-    });
-
-    const userDoneRates = [];
-
-    for (const uSeq of uSeqArray) {
-      const userScore = await GroupUser.findOne({
-        where: { uSeq: uSeq, gSeq: gSeq },
-        attributes: ['guNowScore'],
+    try {
+      // 달성률
+      // 현재 점수 % 그룹 미션 총 점수 *100
+      // 그룹 미션 총 점수
+      const missionTotal = await Group.findOne({
+        where: { gSeq: gSeq },
+        attributes: ['gTotalScore'],
       });
 
-      const userDoneRate =
-        (userScore.guNowScore / missionTotal.gTotalScore) * 100;
-      userDoneRates.push(userDoneRate);
+      const userDoneRates = [];
+
+      for (const uSeq of uSeqArray) {
+        const userScore = await GroupUser.findOne({
+          where: { uSeq: uSeq, gSeq: gSeq },
+          attributes: ['guNowScore'],
+        });
+
+        const userDoneRate =
+          (userScore.guNowScore / missionTotal.gTotalScore) * 100;
+        userDoneRates.push(userDoneRate);
+      }
+      console.log(userDoneRates);
+      return userDoneRates;
+    } catch (error) {
+      // 기타 데이터베이스 오류
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        msg: '서버에러 발생',
+      });
     }
-    console.log(userDoneRates);
-    return userDoneRates;
   },
 
-  ranking: async (gSeq) => {},
+  ranking: async (gSeq) => {
+    // 누적 점수 랭킹
+    // 달성률 랭킹
+  },
 };
