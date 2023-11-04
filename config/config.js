a5d528095a023187439a0efbb995f22d17df3bd3
chitty12 <cty12angel@naver.com>
2023-11-04 11:09:55 +0900

    fix/naverLogin : 로컬/배포 분리

--- config/config.js
+++ config/config.js
@@ -19,6 +19,9 @@ const config = {
     // 서버 설정
     serverUrl: process.env.SERVER_DEV_URL,
     serverPort: process.env.SERVER_DEV_PORT,
+    //로그인 연동 설정
+    naverClientId: process.env.NAVER_CLIENT_ID,
+    naverClientSecret: process.env.NAVER_CLIENT_SECRET,
     // 프론트 설정
     frontPort: process.env.FRONT_DEV_PORT,
   },
@@ -33,6 +36,9 @@ const config = {
     // 서버 설정
     serverUrl: process.env.SERVER_PROD_DOMAIN,
     serverPort: process.env.SERVER_PROD_PORT,
+    //로그인 연동 설정
+    naverClientId: process.env.NAVER_CLIENT_ID_PROD,
+    naverClientSecret: process.env.NAVER_CLIENT_SECRET_PROD,
     // 프론트 설정
     frontPort: process.env.FRONT_PROD_PORT,
   },
