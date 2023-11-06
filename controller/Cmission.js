934c606c879f5c82c31725c3a2c40ad2def91fc6
chitty12 <cty12angel@naver.com>
2023-11-06 23:43:40 +0900

    fix/editMission : node cron 수정

--- controller/Cmission.js
+++ controller/Cmission.js
@@ -10,8 +10,8 @@ const score = require('../modules/rankSystem');
 // 로그인 된 사용자인지 아닌지 판별하려면 불러와야함
 const jwt = require('../modules/jwt');
 
-// 하루가 지나는 날(00:01 분에 업데이트 되는 data)
-cron.schedule('1 0 * * *', async () => {
+// 점수 자동 업데이트 함수
+const updateScore = async () => {
   try {
     // 현재 날짜를 얻기
     const currentDate = new Date();
@@ -50,6 +50,7 @@ cron.schedule('1 0 * * *', async () => {
           { guNowScore: 0 },
           { where: { gSeq: group.gSeq } }
         );
+        // 그룹 미션 점수 초기화
         await Group.update({ gTotalScore: 0 }, { where: { gSeq: group.gSeq } });
         console.log(
           '1. 누적 점수 업데이트!!!! 2. 미션만료!!! 3. 모임 미션 점수 초기화!!!'
@@ -58,12 +59,14 @@ cron.schedule('1 0 * * *', async () => {
     }
   } catch (error) {
     // 기타 데이터베이스 오류
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      msg: '서버에러 발생',
-    });
+    console.log('노드 크론 실행 중 서버 에러', error);
+    // 에러 핸들링 코드 추가
   }
+};
+
+// 하루가 지나는 날(00:01 분에 업데이트 되도록 실행)
+cron.schedule('1 0 * * *', () => {
+  updateScore();
 });
 
 // 유저 미션 리스트
